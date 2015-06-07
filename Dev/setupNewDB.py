b2763df667f1394b682c40f8a1ea359367af5f58
Rick <ikbenrick@gmail.com>
2015-06-07 23:59:56 +0200

    mysql test 2

--- Dev/setupNewDB.py
+++ Dev/setupNewDB.py
@@ -1,7 +1,14 @@
-import MySQLdb as sql
+import MySQLdb
 
-con = sql.connect(username="root", passwd="wortel")
-cur = con.cursor()
-cur.execute('CREATE DATABASE taken;')
+conn = MySQLdb.connect (host = "localhost",
+						user = "root",
+						passwd = "wortel",
+						db = "test")
+cursor = conn.cursor ()
+cursor.execute ("SELECT VERSION()")
+row = cursor.fetchone ()
+print "server version:", row[0]
+cursor.close ()
+conn.close ()
 
 print("much succes")
