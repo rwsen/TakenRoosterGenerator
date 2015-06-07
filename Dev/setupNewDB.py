0fc8e2c3c3df2d5bc74aab0ce6dbb0dcd8c49bcd
Rick <ikbenrick@gmail.com>
2015-06-08 00:21:26 +0200

    maak een tabel

--- Dev/setupNewDB.py
+++ Dev/setupNewDB.py
@@ -1,13 +1,47 @@
 import MySQLdb
 
+#open connection to mysql server
 conn = MySQLdb.connect (host = "localhost",
 						user = "root",
 						passwd = "wortel",
 						db = "test")
 cursor = conn.cursor ()
-cursor.execute ("SELECT VERSION()")
-row = cursor.fetchone ()
-print "server version:", row[0]
+
+#add table taken
+cursor.execute("DROP TABLE IF EXISTS taken")
+cursor.execute("""
+		CREATE TABLE taken
+		(
+			id INT,
+			naam CHAR(40),
+			regelmaat INT
+		)
+	""")
+cursor.execute("""
+		INSERT INTO taken (id, naam, regelmaat)
+		VALUES
+			(1, 'WC maandag of dinsdag', 1),
+			(2, 'WC donderdag of vrijdag', 1),
+			(3, 'Keukenvloer', 1)
+	""")
+print "%d rows were inserted" % cursor.rowcount
+
+cursor.execute ("SELECT id, naam FROM taken")
+while (1):
+	row = cursor.fetchone ()
+	if row == None:
+		break
+	print "%s, %s" % (row[0], row[1])
+print "%d rows were returned" % cursor.rowcount
+
+
+
+#add table mensen
+
+
+
+
+#close connection to mysql server
 cursor.close ()
 conn.close ()
 
