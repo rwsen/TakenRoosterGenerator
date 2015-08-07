170aed54ebb18794873a0789fb18f30727e8ad5a
Rick <ikbenrick@gmail.com>
2015-08-07 21:05:38 +0200

    SQL and datetime
    - now connects to the SQL DB
    - calculates current week and year
    - calculates weeknumer three weeks from now

--- Dev/run.py
+++ Dev/run.py
@@ -1,4 +1,35 @@
+#! /usr/bin/python
+
 #run.py by Rick Sen
 #draai dit script om het takenrooster te genereren
 
-print("hallo, wereld")
+#import MySQLdb
+import datetime
+
+
+#open connection to mysql server
+conn = MySQLdb.connect (host = "localhost",
+						user = "root",
+						passwd = "wortel",
+						db = "test")
+cursor = conn.cursor ()
+
+
+#get weeknumber and year
+dateToday = datetime.date.today()
+currentWeek = dateToday.isocalendar()[1]
+currentYear = dateToday.isocalendar()[0]
+
+#add 21 days to the dateToday, to get a future weeknumber
+dateFuture = dateToday + datetime.timedelta(210)
+futureWeek = dateFuture.isocalendar()[1]
+futureYear = dateFuture.isocalendar()[0]
+
+
+
+#close connection to mysql server
+cursor.close ()
+conn.close ()
+
+
+print("much succes")
