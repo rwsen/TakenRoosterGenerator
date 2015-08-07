a0c32cc56a9bb0d66140cbfc06371ad81e400021
Rick <ikbenrick@gmail.com>
2015-08-07 20:44:02 +0200

    Tabellen toegevoegd
    - takentabel uitgebreid
    - namentabel toegevoegd
    - roostertabel aangemaakt
    - scoretabel aangemaakt

--- Dev/setupNewDB.py
+++ Dev/setupNewDB.py
@@ -22,9 +22,22 @@ cursor.execute("""
 cursor.execute("""
 		INSERT INTO taken (id, naam, regelmaat)
 		VALUES
-			(1, 'WC maandag of dinsdag', 1),
-			(2, 'WC donderdag of vrijdag', 1),
-			(3, 'Keukenvloer', 1)
+			(1, 'Vuilniszakken', 1),
+			(2, 'WC maandag of dinsdag', 1),
+			(3, 'WC donderdag of vrijdag', 1),
+			(4, 'Douchekabines', 1),
+			(5, 'Doucheruimte', 1),
+			(6, 'Keuken: aanrechten', 1),
+			(7, 'Keuken: vloer', 1)
+			(8, 'Keuken: vloer', 1)
+			(9, 'Trappenhuis', 3),
+			(10, 'Trappenhuis', 3),
+			(11, 'Ramen', 8),
+			(12, 'Keuken: kleine dingen', 1),
+			(13, 'Keuken: krattenplank', 2),
+			(14, 'Adelaarstraat', 3),
+			(15, 'Koekoekstraat', 3),
+			(16, 'Lijsterstraat', 3)
 	""")
 print "%d rows were inserted" % cursor.rowcount
 
@@ -39,8 +52,90 @@ print "%d rows were returned" % cursor.rowcount
 
 
 #add table mensen
+cursor.execute("""
+		CREATE TABLE mensen
+		(
+			personID INT,
+			naam CHAR(40)
+		)
+	""")
+cursor.execute("""
+		INSERT INTO mensen (personID, naam)
+		VALUES
+			(1, 'Iris'),
+			(2, 'Emmalien'),
+			(3, 'Manoushka'),
+			(4, 'Annabel'),
+			(5, 'Freek'),
+			(6, 'Wouter'),
+			(7, 'Danique'),
+			(8, 'Laura'),
+			(9, 'Michel'),
+			(10, 'Ilse'),
+			(11, 'Michiel'),
+			(12, 'Elke'),
+			(13, 'Myra'),
+			(14, 'Valentijn'),
+			(15, 'Koen'),
+			(16, 'Limaia'),
+			(17, 'Thijmen'),
+			(18, 'Tim'),
+			(19, 'Paul'),
+			(20, 'Jurriën'),
+			(21, 'Thomas'),
+			(22, 'Rick'),
+			(23, 'Alper'),
+			(24, 'Folkert')
+	""")
+print "%d rows were inserted" % cursor.rowcount
 
+#add table rooster
+cursor.execute("""
+		CREATE TABLE rooster
+		(
+			weeknummer INT,
+			taakID INT,
+			personID INT,
+			uitgevoerd BOOL
+		)
+	""")
 
+#add table score
+cursor.execute("""
+		CREATE TABLE score
+		(
+			personID INT,
+			score INT
+		)
+	""")
+cursor.execute("""
+		INSERT INTO score (personID, score)
+		VALUES
+			(1, 0),
+			(2, 0),
+			(3, 0),
+			(4, 0),
+			(5, 0),
+			(6, 0),
+			(7, 0),
+			(8, 0),
+			(9, 0),
+			(10, 0),
+			(11, 0),
+			(12, 0),
+			(13, 0),
+			(14, 0),
+			(15, 0),
+			(16, 0),
+			(17, 0),
+			(18, 0),
+			(19, 0),
+			(20, 0),
+			(21, 0),
+			(22, 0),
+			(23, 0),
+			(24, 0)
+	""")
 
 
 #close connection to mysql server
