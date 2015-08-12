8e7736005d7bea908fa0add71334e37f14110a2e
Rick <ikbenrick@gmail.com>
2015-08-12 17:08:27 +0200

    logic error

--- Dev/run.py
+++ Dev/run.py
@@ -49,7 +49,7 @@ def getWorkers(amount, cursor):
 		row = cursor.fetchone ()
 		if row == None:
 			break
-		if not row[0] in IDList: #test availability
+		if row[0] in IDList: #test availability
 			tempTuple = (int(row[1]), int(row[0]))
 			scoreIDsList.append(tempTuple)
 			
